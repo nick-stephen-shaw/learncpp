8a986409107f7b447720743533a9d6393b682d4f
Nick Shaw <nick.stephen.shaw@gmail.com>
2026-06-28 13:57:44 -0400

    Added line breaks

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 int main() {
+    int x{10};
     std::cout << "Hello, World!";
+    std::cout << "\n";
     std::cout << "And beyond";
+    std::cout << "\n";
+    std::cout << x;
     return 0;
 }
